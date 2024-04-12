ba9a76362bff77ed4c54e1827a29dd65e08ac580
yaqiang.sun <sunyaking@163.com>
2024-04-12 01:33:30 -0400

    add init file

---
+++ cli_demo.py
@@ -0,0 +1,18 @@
+# Copyright (c) 2024 yaqiang.sun.
+# 
+# This source code is licensed under the license found in the LICENSE file 
+# in the root directory of this source tree.
+
+"""A command-line interactive chat demo"""
+
+import os
+import argparse
+import platform
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
