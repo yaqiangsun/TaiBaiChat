1d84b57573e6a690be983ecd4237debf1c3f873c
yaqiang.sun <sunyaking@163.com>
2024-04-19 04:59:58 -0400

    update model inference config

--- model.py
+++ model.py
@@ -7,6 +7,17 @@
 
 from transformers import AutoModelForCausalLM, AutoTokenizer, TextIteratorStreamer
 from threading import Thread
+from transformers import GenerationConfig
+
+from transformers import InfNanRemoveLogitsProcessor, LogitsProcessorList
+def get_logits_processor() -> "LogitsProcessorList":
+    r"""
+    Gets logits processor that removes NaN and Inf logits.
+    """
+    logits_processor = LogitsProcessorList()
+    logits_processor.append(InfNanRemoveLogitsProcessor())
+    return logits_processor
+
 
 class TaibaiChat:
     def __init__(self,checkpoint_path:str=None,cpu_only:bool=False) -> None:
@@ -35,7 +46,16 @@ class TaibaiChat:
         model = self.model
         tokenizer = self.tokenizer
         conversation = [
-            {'role': 'system', 'content': """You are a helpful assistant."""},
+            {'role': 'system', 'content': 
+                        "You are a helpful, respectful and honest assistant. "
+                        "Always answer as helpfully as possible, while being safe. "
+                        "Your answers should not include any harmful, unethical, "
+                        "racist, sexist, toxic, dangerous, or illegal content. "
+                        "Please ensure that your responses are socially unbiased and positive in nature.\n\n"
+                        "If a question does not make any sense, or is not factually coherent, "
+                        "explain why instead of answering something not correct. "
+                        "If you don't know the answer to a question, please don't share false information."
+             },
         ]
         for query_h, response_h in history:
             conversation.append({'role': 'user', 'content': query_h})
@@ -54,9 +74,18 @@ class TaibaiChat:
         # print("inputs_word:\n",inputs_word,"\n end.")
         inputs = inputs.to(model.device)
         streamer = TextIteratorStreamer(tokenizer=tokenizer, skip_prompt=True, timeout=60.0, skip_special_tokens=True)
+        generating_args = {}
+        generating_args["do_sample"] = True
+        generating_args["temperature"] = 0.95
+        generating_args["top_p"] =  0.7
+        generating_args["pad_token_id"] = 32000
+        generating_args["eos_token_id"] = [2]
+        generating_args["max_new_tokens"] = 512
         generation_kwargs = dict(
             input_ids=inputs,
             streamer=streamer,
+            logits_processor=get_logits_processor(),
+            generation_config=GenerationConfig(**generating_args),
         )
         thread = Thread(target=model.generate, kwargs=generation_kwargs)
         thread.start()
