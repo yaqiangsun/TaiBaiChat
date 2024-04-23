7fc9579615d6d8c8eaf74a4ac1b88da7d756f38a
yaqiang.sun <sunyaking@163.com>
2024-04-22 23:29:35 -0400

    update prompt

--- model.py
+++ model.py
@@ -47,14 +47,15 @@ class TaibaiChat:
         tokenizer = self.tokenizer
         conversation = [
             {'role': 'system', 'content': 
-                        "You are a helpful, respectful and honest assistant. "
-                        "Always answer as helpfully as possible, while being safe. "
-                        "Your answers should not include any harmful, unethical, "
-                        "racist, sexist, toxic, dangerous, or illegal content. "
-                        "Please ensure that your responses are socially unbiased and positive in nature.\n\n"
-                        "If a question does not make any sense, or is not factually coherent, "
-                        "explain why instead of answering something not correct. "
-                        "If you don't know the answer to a question, please don't share false information."
+                        "You are a helpful assistant. 你是一个乐于助人的助手。"
+                        # "You are a helpful, respectful and honest assistant. "
+                        # "Always answer as helpfully as possible, while being safe. "
+                        # "Your answers should not include any harmful, unethical, "
+                        # "racist, sexist, toxic, dangerous, or illegal content. "
+                        # "Please ensure that your responses are socially unbiased and positive in nature.\n\n"
+                        # "If a question does not make any sense, or is not factually coherent, "
+                        # "explain why instead of answering something not correct. "
+                        # "If you don't know the answer to a question, please don't share false information."
              },
         ]
         for query_h, response_h in history:
